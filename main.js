ab5742adc25d7afc569f6b94ae26e96372144121
Orvin L <108455291+Lidnch@users.noreply.github.com>
2024-06-04 12:08:54 +0700

    Sidebar rework and form layout
    Implement sidebar rework (html and css) to all pages. Separating script to main.js with new value increment-decrement button script.

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// Add JS here
+const body = document.querySelector('body'),
+        sidebar = body.querySelector('nav'),
+        toggle = body.querySelector(".toggle"),
+        searchBtn = body.querySelector(".search-box"),
+        modeSwitch = body.querySelector(".toggle-switch"),
+        modeText = body.querySelector(".mode-text");
+        toggle.addEventListener("click" , () =>{
+            sidebar.classList.toggle("close");
+        })
+        searchBtn.addEventListener("click" , () =>{
+            sidebar.classList.remove("close");
+        });
+
+
+const plus = document.querySelector(".plus"),
+num = document.querySelector(".num"),
+minus = document.querySelector(".minus");
+
+let a = 1;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    num.innerHTML = a;
+    console.log(a);
+})
+
+minus.addEventListener("click", ()=>{
+    if(a > 0){
+        a--;
+    }
+    num.innerHTML = a;
+    console.log(a);
+})
